9bf7968ddf09c5f911cb818adf0d997a02ad6458
KAMESH\kamesh_chandrasekar <kamesh1164@gmail.com>
2018-11-16 01:05:44 +0530

    [change] removing flake8 error

--- sms.py
+++ sms.py
@@ -9,6 +9,7 @@ parser.add_argument('--user', type=str, help='The User name of way2sms', require
 parser.add_argument('--pass', type=str, help='Password of the account of user', required=True, dest='paswd')
 data = parser.parse_args()
 
+
 def collect():
     
     message = data.message
@@ -17,32 +18,35 @@ def collect():
     passwd = data.paswd
     return username,passwd,number,message
 
-def send_sms(username,passwd,number,message=''):
+
+def send_sms(username, passwd, number, message=''):
 
     url = 'http://www.way2sms.com/re-login'
     data = 'mobileNo='+username+'&password='+passwd+'&CatType'
     cj = http.cookiejar.CookieJar()
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-    opener.addheaders = [('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')]             
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')]             
+
     try:
         opener.open(url, data.encode('utf-8'))
-    except:
-        print('Cannot connect a connection')
+    except Exception as e:
+        print('Cannot connect a connection becasuse ' + str(e))
         return False
     
     session_id = str(cj).split('~')[1].split(' ')[0]
     smsurl = 'http://www.way2sms.com/smstoss'
     data = 'ssaction=ss&Token='+session_id+'&toMobile='+number+'&message='+message
     opener.addheaders = [('Referer', 'http://www.way2sms.com/send-sms+'+session_id)]             
+
     try:
-        opener.open(smsurl,data.encode('utf-8'))
+        opener.open(smsurl, data.encode('utf-8'))
         print('Success')
-    except:
-        print('SMS cannot be sent')
+    except Exception as e:
+        print('SMS cannot be sent because' + str(e))
         return False            
     return True
 
 if __name__ == "__main__":
-    username,passwd,number,message = collect()
-    choice = send_sms(username,passwd,number,message)
+    username, passwd, number, message = collect()
+    choice = send_sms(username, passwd, number, message)
     print(choice)
